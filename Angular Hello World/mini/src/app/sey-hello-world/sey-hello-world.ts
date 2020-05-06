70c5c4e3a84eeb099e0a5fa9e9ef0c1f5ecf73fc
katescode <katecode@protonmail.com>
2020-05-06 08:13:08 +0300

    empty minimal

--- Angular Hello World/mini/src/app/sey-hello-world/sey-hello-world.ts
+++
@@ -1,7 +0,0 @@
-export class SeyHelloWorld {
-    constructor(public sayHello:string,
-        public sayWorld:string){
-         
-        }
-    GetHelloWotld(){return `${this.sayHello} ${this.sayWorld}!` }
-}
--- Angular Hello World/my fitst project/readme.md
+++
@@ -1,9 +0,0 @@
-## for create new  angular project
-ng new newProject
-
-## for run
-ng serve
-
-## in browser
-localhost:4200
-
